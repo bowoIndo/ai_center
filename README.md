adb58625b92762fcee57ea596ec30c2b6dc43515
optimusbow <optimusbow@gmail.com>
2025-03-30 10:55:24 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ai_center
 ai_center
+
+list free api ai CHAT
+
+No	Nama AI Chat Server	Website	API Gratis / Free Tier	Catatan Penggunaan
+1	Hugging Face	huggingface.co	✅ Gratis	Banyak model NLP gratis, perlu API token
+2	OpenAI (GPT-3.5)	platform.openai.com	🔄 Free trial ($5-$18)	Free trial saja, setelah itu berbayar
+3	Cohere	cohere.com	✅ Free tier	Text generation, classification, search
+4	Open-Assistant	open-assistant.io	✅ Gratis	Open-source, bisa self-hosted atau API publik
+5	ChatGPT Clone di GitHub	GitHub	✅ Gratis	Banyak proyek open-source, perlu setup sendiri
+6	DeepAI	deepai.org	✅ Gratis	Batas penggunaan harian
+7	AI21 Labs (Jurassic-2)	ai21.com	🔄 Free tier	Text generation, mirip GPT-3
+8	Wit.ai (Meta)	wit.ai	✅ Gratis	NLP untuk pengenalan suara dan teks, tanpa batasan
+9	Rasa	rasa.com	✅ Gratis	Framework open-source, butuh self-hosting
+10	DialoGPT (Microsoft)	GitHub	✅ Gratis	Bisa diakses via Hugging Face, model dialog NLP
+
+
+###############################################################
+
+cara daftar Hugging Face gratis
+
--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from datetime import datetime
 import konfigurasi
 from groq import Groq
 from flask_migrate import Migrate
-
+import json
+from openai import OpenAI
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bowo:mn@localhost:5432/aisuper'
@@ -17,6 +19,8 @@ class InputOutput(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pertanyaan = db.Column(db.Text, nullable=False)
     jawaban = db.Column(db.Text, nullable=False)
+    server_ai = db.Column(db.Text, nullable=False, default='')
+    is_pembuat_kesimpulan = db.Column(db.Boolean, default=False)
     created_timestamp = db.Column(db.DateTime, default=datetime.now, nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -26,6 +30,247 @@ def index():
         email = request.form.get('email')
         return f"Form submitted! Name: {name}, Email: {email}"
     return render_template('form.html')
+
+
+@app.route('/kesimpulan', methods=['POST'])
+def kesimpulan():
+    searchbox = request.form.get('searchbox')
+    hasil = InputOutput.query.filter_by(pertanyaan=searchbox).all()
+    tampungan_jawaban = '' # menampung semua jawaban di pisah memakai spasi antar jawaban
+    if hasil:
+        for row in hasil:
+            tampungan_jawaban = ' '.join([row.jawaban for row in hasil])
+
+    pertanyaan_ke_deepseek = 'buat kesimpulan dari : ' + tampungan_jawaban
+
+    # insert pertanyaan = pertanyaan_ke_deepseek , server_ai = 'deepseek' , is_pembuat_kesimpulan = True ke table input_output,setelah sukses insert ambil id input_output yang baru di input
+    jawaban = 'jawaban kesimpulan dari deepseek'
+
+    data_baru = InputOutput(
+        pertanyaan=pertanyaan_ke_deepseek,
+        jawaban = jawaban,  
+        server_ai='deepseek',
+        is_pembuat_kesimpulan=True
+    )
+
+    db.session.add(data_baru)
+    db.session.commit()
+
+    # Ambil ID dari input baru
+    id_baru = data_baru.id
+
+    # update isi kesimpulan_id = id_baru dari setiap data hasil
+    for row in hasil:
+        row.kesimpulan_id = id_baru
+    
+    db.session.commit()  # Simpan perubahan ke database
+
+    # client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
+
+    # response = client.chat.completions.create(
+    #     model="deepseek-chat",
+    #     messages=[
+    #         {"role": "system", "content": "You are a helpful assistant"},
+    #         {"role": "user", "content": searchbox},
+    #     ],
+    #     stream=False
+    # )
+    # jawaban = response.choices[0].message.content
+
+    return jawaban  
+
+
+@app.route('/you', methods=['POST'])
+def you():
+
+    searchbox = request.form.get('searchbox')
+
+    headers = {"X-API-Key": konfigurasi.YOU_API_KEY}
+    params = {"query": searchbox}
+    jawaban = requests.get(
+        "https://chat-api.you.com/search?query={query}",
+        params=params,
+        headers=headers,
+    ).json()
+
+    data = InputOutput(pertanyaan=searchbox, jawaban=jawaban, server_ai='you')
+    db.session.add(data)
+    db.session.commit()
+
+    return jsonify({"pertanyaan": searchbox, "jawaban": jawaban})
+
+
+
+@app.route('/grok', methods=['POST'])
+def grok():
+
+
+    searchbox = request.form.get('searchbox')
+
+    client = OpenAI(
+        api_key=konfigurasi.GROK_API_KEY,
+        base_url="https://api.x.ai/v1",
+    )
+
+
+
+    completion = client.chat.completions.create(
+        model="grok-beta",
+        messages=[
+            {"role": "system", "content": "You are Grok, a chatbot inspired by the Hitchhikers Guide to the Galaxy."},
+            {"role": "user", "content": "Explain the risks of improper AI usage and its contribution to autocratic regimes."},
+        ],
+    )
+
+    jawaban=completion.choices[0].message
+
+    data = InputOutput(pertanyaan=searchbox, jawaban=jawaban, server_ai='grok')
+    db.session.add(data)
+    db.session.commit()
+
+    return jsonify({"pertanyaan": searchbox, "jawaban": jawaban})
+
+
+
+
+
+
+@app.route('/perplexity', methods=['POST'])
+def perplexity():
+
+    searchbox = request.form.get('searchbox')
+    messages = [
+        {
+            "role": "system",
+            "content": (
+                "You are an artificial intelligence assistant and you need to "
+                "engage in a helpful, detailed, polite conversation with a user."
+            ),
+        },
+        {   
+            "role": "user",
+            "content": (
+                searchbox
+            ),
+        },
+    ]
+    client = OpenAI(api_key=konfigurasi.PERPLEXITY_API_KEY, base_url="https://api.perplexity.ai")
+    response = client.chat.completions.create(
+        model="sonar-pro",
+        messages=messages,
+    )
+
+    data = InputOutput(pertanyaan=searchbox, jawaban=response, server_ai='perplexity')
+    db.session.add(data)
+    db.session.commit()
+
+    return jsonify({"pertanyaan": searchbox, "jawaban": response})
+
+
+
+@app.route('/gemini', methods=['POST'])
+def gemini():
+    searchbox = request.form.get('searchbox')
+
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3:generateContent?key={konfigurasi.GEMINI_API_KEY}"
+
+    payload = {
+        "contents": [
+            {
+                "text": searchbox
+            }
+        ]
+    }
+
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        jawaban = response.json()
+
+        # result = response.json()
+        # answer = result['choices'][0]['message']['content']
+        # jawaban = answer.strip()
+
+    else:
+        response_dict = json.loads(response.text)
+        jawaban = response_dict['error']['message']
+
+
+
+    data = InputOutput(pertanyaan=searchbox, jawaban=jawaban, server_ai='gemini')
+    db.session.add(data)
+    db.session.commit()
+
+    return jsonify({"pertanyaan": searchbox, "jawaban": jawaban})
+
+
+@app.route('/chatgpt', methods=['POST'])
+def chatgpt():
+    searchbox = request.form.get('searchbox')
+
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {konfigurasi.CHATGPT_API_KEY}"
+    }
+
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "user", "content": searchbox}
+        ],
+        "max_tokens": 100
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        result = response.json()
+        # Ambil konten teks dari response
+        answer = result['choices'][0]['message']['content']
+        jawaban = answer.strip()
+
+    else:
+        response_dict = json.loads(response.text)
+        jawaban = response_dict['error']['message']
+
+
+
+    data = InputOutput(pertanyaan=searchbox, jawaban=jawaban, server_ai='chatgpt')
+    db.session.add(data)
+    db.session.commit()
+
+    return jsonify({"pertanyaan": searchbox, "jawaban": jawaban})
+
+
+@app.route('/huggingface', methods=['POST'])
+def huggingface():
+    searchbox = request.form.get('searchbox')
+
+    url = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct"
+    headers = {
+        "Authorization": f"Bearer {konfigurasi.HUGGINGFACE_API_KEY}"
+    }
+    data = {
+        "inputs": searchbox
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    data = json.loads(response.text)
+    jawaban = data[0]["generated_text"]
+
+    data = InputOutput(pertanyaan=searchbox, jawaban=jawaban, server_ai='hugging_face')
+    db.session.add(data)
+    db.session.commit()
+
+    return jsonify({"pertanyaan": searchbox, "jawaban": jawaban})
+
+
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -44,8 +289,7 @@ def search():
 
         jawaban_server = chat_completion.choices[0].message.content
 
-        # Simpan hasil pencarian ke database
-        data = InputOutput(pertanyaan=searchbox, jawaban=jawaban_server)
+        data = InputOutput(pertanyaan=searchbox, jawaban=jawaban_server, server_ai='groq')
         db.session.add(data)
         db.session.commit()
 
@@ -56,63 +300,3 @@ def search():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-# import requests
-# from flask import Flask, render_template, request,jsonify
-# import os
-# from groq import Groq
-# from flask_sqlalchemy import SQLAlchemy
-# from datetime import datetime
-# import konfigurasi
-
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bowo:mn@localhost:5432/aisuper'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-
-# class InputOutput(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     pertanyaan = db.Column(db.Text, nullable=False)
-#     jawaban = db.Column(db.Text, nullable=False)
-#     created_timestamp = db.Column(db.DateTime, default=datetime.now, nullable=False)
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         name = request.form.get('name')
-#         email = request.form.get('email')
-#         return f"Form submitted! Name: {name}, Email: {email}"
-#     return render_template('form.html')
-
-
-# @app.route('/search', methods=['POST'])
-# def search():
-#     searchbox = request.form.get('searchbox')
-
-#     client = Groq(api_key=konfigurasi.GROQ_API_KEY)
-
-#     chat_completion = client.chat.completions.create(
-#         messages=[
-#             {
-#                 "role": "user",
-#                 "content": "Explain the importance of low latency LLMs",
-#             }
-#         ],
-#         model="llama3-8b-8192",
-#     )
-
-#     jawaban_server = chat_completion.choices[0].message.content
-
-#     data = InputOutput(pertanyaan=searchbox, jawaban=jawaban_server)
-#     db.session.add(data)
-#     db.session.commit()
-
-#     return jawaban_server
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
